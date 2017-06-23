9188ea67b2fbc916c230f9f46cfd2f0a06ccf317
Maria del Rosario Palomino Begazo <100kj.mr@gmail.com>
2017-06-23 15:54:47 -0700

    Ejercicios con objetos

---
+++ index.js
@@ -0,0 +1,98 @@
+var assert = require("assert");
+//EJERCICIO 1
+/*function Rectangulo(){
+  this.area = function(base,altura){
+    this.base = base;
+    this.altura = altura;
+    return this.base * this.altura;
+  };
+}
+*/
+function Rectangulo(base,altura){
+  this.base = base;
+  this.altura = altura;
+  this.area = function(){
+
+    return (this.base * this.altura);
+  };
+}
+
+//EJERCICIOS 2
+/*function Persona(){
+  this.calcularEdad = function(nombre, dia, mes, a_nacim){
+    this.nombre = nombre;
+    this.dia = dia;
+    this.mes = mes;
+    this.a_nacim = a_nacim;
+    this.annio_actual=2017;
+    return ("La edad actual de "+this.nombre + " es " + (this. annio_actual - this.a_nacim));
+  }
+}*/
+function Persona(nombre,dia,mes,a_nacim){
+  this.nombre = nombre;
+  this.dia = dia;
+  this.mes = mes;
+  this.a_nacim = a_nacim;
+  this.annio_actual=2017;
+  this.calcularEdad = function(){
+
+    return ("La edad actual de "+this.nombre + " es " + (this. annio_actual - this.a_nacim));
+  }
+}
+
+//EJERCICIO 3
+/*function Humano(){
+  this.hablar = function(nombre,edad,pasatiempo){
+    this.nombre = nombre;
+    this.edad = edad;
+    this.pasatiempo = pasatiempo;
+    return ("Hola soy " + this.nombre + " tengo " + this.edad+" años y me gusta "+this.pasatiempo);
+  }
+}*/
+function Humano(nombre,edad,pasatiempo){
+    this.nombre = nombre;
+    this.edad = edad;
+    this.pasatiempo = pasatiempo;
+    this.hablar = function(){
+          return ("Hola soy " + this.nombre + " tengo " + this.edad+" años y me gusta "+this.pasatiempo);
+    }
+}
+
+
+
+
+/*var rectangulo = new Rectangulo();
+var persona = new Persona();
+var humano = new Humano();*/
+
+
+var rectangulo = new Rectangulo(3,4);
+var persona = new Persona("Sú",23,8,1991);
+var humano = new Humano("Maria", 24, "bailar");
+
+describe("Ejercicios Objetos", function(){
+  describe("Ejercicio 1", function(){
+    it("Prueba con altura y base", function(){
+      //assert.equal(12,rectangulo.area(3,4));
+        assert.equal(12,rectangulo.area());
+    });
+  });
+
+  describe("Ejercicio 2", function(){
+     it("Prueba con Datos", function(){
+       //assert.equal("La edad actual de Sú es 26",persona.calcularEdad("Sú",23,8,1991));
+        assert.equal("La edad actual de Sú es 26",persona.calcularEdad());
+     });
+
+  });
+
+  describe("Ejercicio 3", function(){
+     it("Prueba con Datos", function(){
+       //assert.equal("Hola soy Maria tengo 24 años y me gusta bailar",humano.hablar("Maria", 24, "bailar"));
+         assert.equal("Hola soy Maria tengo 24 años y me gusta bailar",humano.hablar());
+
+     });
+
+  });
+
+});
